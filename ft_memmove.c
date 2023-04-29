81366d5d38f8644222a378cc877ff1f9993dbdea
Meng Seong Lee <melee@u81z03s02.42kl.edu.my>
2023-04-29 15:44:35 +0800

    done

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/29 13:25:28 by melee             #+#    #+#             */
-/*   Updated: 2023/04/29 15:28:15 by melee            ###   ########.fr       */
+/*   Created: 2023/04/29 13:56:41 by melee             #+#    #+#             */
+/*   Updated: 2023/04/29 14:26:12 by melee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dst, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*dst_char;
 	char	*src_char;
@@ -21,24 +21,24 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	i = 0;
 	dst_char = (char *)dst;
 	src_char = (char *)src;
-	while ((dst || src) && i < n)
+	while ((dst || src) && i < len)
 	{
 		dst_char[i] = src_char[i];
 		i++;
 	}
 	return (dst);
 }
-/*
+
 #include <string.h>
 #include <stdio.h>
 
 int	main(void)
 {
-	char src[]="abcdefgh";
+	char src[]="foo-bar";
 	//char dst[] = "goodbye";
-	//ft_memcpy(&src[5], &src[3], 3);
-	memcpy(&src[5], &src[3], 3);
+	memcpy(&src[4], &src[4], 4);
+//	ft_memmove(&src[3], &src[4], 4);
 	printf("%s",src);
 	return (0);
 }
-*/
+
