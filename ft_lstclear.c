a8cc48372b26357221e248fdbc4cb6f512c5cb6f
Meng Seong Lee <melee@u81z05s02.42kl.edu.my>
2023-05-04 15:18:09 +0800

    ft_lstclear

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: melee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/04 13:47:47 by melee             #+#    #+#             */
-/*   Updated: 2023/05/04 14:15:39 by melee            ###   ########.fr       */
+/*   Created: 2023/05/04 14:11:07 by melee             #+#    #+#             */
+/*   Updated: 2023/05/04 15:16:13 by melee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstdelone(t_list *lst, void (*del)(void*))
+void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	if (lst && del)
+	t_list	*node;
+	t_list	*node_delete;
+
+	if (lst && *lst && del)
 	{
-		if (lst->content)
-			del(lst->content);
-		free(lst);
+		node = *lst;
+		while (node)
+		{
+			node_delete = node;
+			node = node->next;
+			ft_lstdelone(node_delete, del);
+		}
+		*lst = (NULL);
 	}
 }
