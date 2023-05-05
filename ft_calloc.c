641d97906acbc089a37085af6e17791311558803
Meng Seong Lee <melee@u81z05s02.42kl.edu.my>
2023-05-05 13:01:00 +0800

    all 3 test pass everything except for strnstr aleliever

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,12 @@
 /*   By: melee <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/01 18:18:50 by melee             #+#    #+#             */
-/*   Updated: 2023/05/05 12:04:28 by melee            ###   ########.fr       */
+/*   Updated: 2023/05/05 12:57:34 by melee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 void	*ft_calloc(size_t count, size_t size)
 {
@@ -18,6 +19,8 @@ void	*ft_calloc(size_t count, size_t size)
 	size_t	i;
 
 	i = 0;
+	if (count >= SIZE_MAX || size >= SIZE_MAX)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
